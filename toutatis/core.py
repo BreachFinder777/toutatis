b64e9f7d3d1e671c16407fa72eb8c7552a91ab38
BreachFinder777 <sohamdatta343@gmail.com>
2026-05-18 11:05:49 +0530

    Update User-Agent header for Instagram API request

--- toutatis/core.py
+++ toutatis/core.py
@@ -12,7 +12,7 @@ import pycountry
 
 
 def getUserId(username, sessionsId):
-    headers = {"User-Agent": "iphone_ua", "x-ig-app-id": "936619743392459"}
+    headers = {"User-Agent": "Instagram 275.0.0.27.98 Android"}
     api = requests.get(
         f'https://i.instagram.com/api/v1/users/web_profile_info/?username={username}',
         headers=headers,
